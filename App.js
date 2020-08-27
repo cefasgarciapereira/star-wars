82650aa14480341a0d269d324515643f4ebc2a4b
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2020-08-27 00:53:37 -0300

    Main component

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes/index';
+import { NavigationContainer } from '@react-navigation/native';
+import {SwapiProvider} from './src/contexts/swapi';
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SwapiProvider>
+        <Routes/>
+      </SwapiProvider>
+    </NavigationContainer>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
